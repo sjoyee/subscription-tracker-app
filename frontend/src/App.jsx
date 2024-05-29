30eace10fed8c6d3e26d9e03c2220cd2b616900e
Siang Jo Yee <joyeesiang@gmail.com>
2024-05-30 01:35:19 +0800

    Added code for dashboard page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import DashboardPage from "./pages/DashboardPage";
+import { useEffect } from "react";
+import SubscriptionPage from "./pages/SubscriptionPage";
 
 function App() {
+  useEffect(() => {
+    const loggedIn = localStorage.getItem("token");
+    if (
+      window.location.pathname !== "/login" &&
+      window.location.pathname !== "/create" &&
+      !loggedIn
+    ) {
+      window.location.replace("/login");
+    }
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/" element={<DashboardPage />} />
+        <Route path="/subscription" element={<SubscriptionPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,4 +7,44 @@ const api = axios.create({
   },
 });
 
+// Add a request interceptor
+api.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+// api.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   async (error) => {
+//     const originalRequest = error.config;
+//     if (error.response.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
+//       const refreshToken = localStorage.getItem('refreshToken');
+//       try {
+//         const response = await axios.post('http://localhost:5000/api/refresh-token', { token: refreshToken });
+//         if (response.status === 200) {
+//           localStorage.setItem('accessToken', response.data.accessToken);
+//           axios.defaults.headers.common['Authorization'] = 'Bearer ' + response.data.accessToken;
+//           return axiosInstance(originalRequest);
+//         }
+//       } catch (error) {
+//         // Handle refresh token failure (e.g., logout user)
+//         console.error('Token refresh failed:', error);
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
 export default api;
